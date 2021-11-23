962a4769762ce0635349ba508bfa66012f8a0c1a
Daisuke Saito <dsaito85a@gmail.com>
2021-11-23 19:31:03 +0900

    [Add] Ratioのサンプルページを作成

---
+++ src/components/Form.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable react/display-name */
+// TODO:react/display-nameについて調べる
+import React, { useEffect } from "react";
+import { useForm, UseFormReturn, SubmitHandler } from "react-hook-form";
+import { useMergeRefs } from "../hooks/useMergeRefs";
+
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    const internalRef = React.useRef<HTMLInputElement>();
+    useEffect(() => {
+      // iPhoneでradio/checkboxのinputにfocusが当たらない問題のワークアラウンド
+      if (props.type !== "radio" && props.type !== "checkbox") {
+        return;
+      }
+      if (!/iPad|iPhone/.test(navigator.userAgent)) {
+        return;
+      }
+
+      internalRef.current?.addEventListener("focus", (e) => {
+        (e.target as HTMLInputElement).scrollIntoView({
+          block: "center",
+          behavior: "smooth",
+        });
+      });
+    }, []);
+    return <input ref={useMergeRefs(internalRef, ref)} {...props} />;
+  }
+);
+
+type Option = {
+  label: React.ReactNode;
+  value: string | number | string[];
+};
+
+type SelectProps = React.DetailedHTMLProps<
+  React.SelectHTMLAttributes<HTMLSelectElement>,
+  HTMLSelectElement
+> & { options: Option[] };
+
+export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ options, ...props }, ref) => (
+    <select ref={ref} {...props}>
+      {options.map(({ label, value }, i) => (
+        <option key={i} value={value}>
+          {label}
+        </option>
+      ))}
+    </select>
+  )
+);
+
+type FormProps<TFormValues> = {
+  onSubmit: SubmitHandler<TFormValues>;
+  children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
+};
+
+export const Form = <
+  TFormValues extends Record<string, any> = Record<string, any>
+>({
+  onSubmit,
+  children,
+}: FormProps<TFormValues>) => {
+  const methods = useForm<TFormValues>();
+  return (
+    <form onSubmit={methods.handleSubmit(onSubmit)}>{children(methods)}</form>
+  );
+};
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  sex: string;
+};
